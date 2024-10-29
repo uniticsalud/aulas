f3fcb0fe97269102d0dea5794a395ef134f6e0cd
uniticsalud <uniticsalud@us.es>
2024-10-29 13:37:13 +0100

    Delete autostart_firefox.ps1

--- autostart_firefox.ps1
+++
@@ -1,9 +0,0 @@
-$browserPath = "C:\Program Files\Mozilla Firefox\firefox.exe"  # Ruta del navegador, actualízala si es necesario
-
-while ($true) {
-    $process = Get-Process -Name firefox -ErrorAction SilentlyContinue
-    if (-not $process) {
-        Start-Process $browserPath
-    }
-    Start-Sleep -Seconds 2
-}
